5dadca44418c616c95279eeb31edc8e347792565
ascender1729 <pavankumar.dubasi2019@gmail.com>
2024-07-06 09:10:05 +0530

    Update main.py to use simplified imports

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 import logging
 from dotenv import load_dotenv
-from src.github_stats import GitHubStats
-from src.readme_updater import ReadmeUpdater
-from src.exceptions import GitHubStatsError
+from src import GitHubStats, ReadmeUpdater, GitHubStatsError
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
